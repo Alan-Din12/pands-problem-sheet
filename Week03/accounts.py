c6b72c5092d15e72931193501f541252dff26a02
Alan Dineen <alandineen@Alans-MacBook-Pro.local>
2023-02-09 15:34:06 +0000

    Week3 Task

---
+++ Week03/accounts.py
@@ -0,0 +1,12 @@
+#accounts.py
+#Author : Alan Dineen
+#This is a program that reads a 10 character accunt number and outputs the account number with only the last 4 digits showing 
+# with the first 6 being displayed as "XXXXXX"
+
+account = input("Please enter your 10 digit account number: ")
+
+print(" XXXXXX" + account[6:])
+
+
+
+
